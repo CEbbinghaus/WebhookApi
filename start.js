079d4fd93f809d8e9c7f457929d031a5f63250dd
Unknown <jecebbi@gmail.com>
2018-01-19 23:37:00 +1100

    Changed Startup Command

--- start.js
+++ start.js
@@ -1,3 +1,5 @@
 const child = require("child_process");
-child.exec("pm2 start server.js --name='API' --watch")
-process.exit(0)
+child.exec("pm2 start server.js --name='API' --watch", (e, out, err) => {
+    console.log("Started Bot")
+    process.exit(0);
+})
